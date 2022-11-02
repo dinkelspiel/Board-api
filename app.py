bde5958a5c375d86233f640e7eab2259902c74ed
shykeiichi <imdark200@gmail.com>
2022-11-03 00:05:01 +0100

    add

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ def getposts():
     if(startint == endint + 1):
         return Response(json.dumps("No more posts"), status=204, mimetype="application/json")
     
-    mycursor.execute(f"SELECT * FROM board ORDER BY id desc limit 10,{startint}")
+    mycursor.execute(f"SELECT * FROM board ORDER BY id desc limit 10 OFFSET s{startint}")
 
     myresult = mycursor.fetchall()
     
