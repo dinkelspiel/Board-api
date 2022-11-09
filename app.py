04405982fa01b0e1ff54b596f3bc024901dfe038
shykeiichi <imdark200@gmail.com>
2022-11-09 12:30:49 +0100

    progrss

--- app.py
+++ app.py
@@ -379,8 +379,8 @@ def ratepost():
     if post == None:
         return Response(json.dumps("Post was not found"), status=500, mimetype="application/json")
     
-    print(f"{rating[4]} {0 if rating_ == False else 1}")
     if rating != None:
+        print(f"{rating[4]} {0 if rating_ == False else 1}")
         if rating[4] == (0 if rating_ == False else 1):
             return Response("Nothing changed", status=204)
     
